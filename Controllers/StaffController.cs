5ca68ed30c323407c19c1ebdfc3fc4c869e1c4fb
azeezsaliu <azeezsaliu108@gmail.com>
2023-09-06 10:14:31 +0100

    Add project files.

---
+++ Controllers/StaffController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StaffManagement.Controllers
+{
+    public class StaffController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HeaderName = "Elev8 Index";
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            ViewData["HeaderName"] = "Elev8 List";
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            TempData["HeaderName"] = "Elev8 Create";
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
